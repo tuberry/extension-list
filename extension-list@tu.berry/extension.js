33bbbebdd08a2fe465c7db5775112d14c27ee435
tuberry <17917040+tuberry@users.noreply.github.com>
2020-11-30 23:31:38 +0800

    refactor

--- extension-list@tu.berry/extension.js
+++ extension-list@tu.berry/extension.js
@@ -5,7 +5,7 @@ const Main = imports.ui.main;
 const Util = imports.misc.util;
 const PanelMenu = imports.ui.panelMenu;
 const PopupMenu = imports.ui.popupMenu;
-const { Shell, GLib, St, GObject, Meta } = imports.gi;
+const { Shell, GLib, St, GObject, Meta, Gio } = imports.gi;
 
 const ExtensionUtils = imports.misc.extensionUtils;
 const ExtDownloader = imports.ui.extensionDownloader;
@@ -56,30 +56,17 @@ var PopupScrollMenu = class extends PopupMenu.PopupMenuSection {
     }
 };
 
-const ExtensionList = GObject.registerClass(
-class ExtensionList extends GObject.Object {
+const ExtensionList = GObject.registerClass({
+    Properties: {
+        'prefs':    GObject.param_spec_boolean('prefs',    '', '', false, GObject.ParamFlags.READWRITE),
+        'url':      GObject.param_spec_boolean('url',      '', '', false, GObject.ParamFlags.READWRITE),
+        'delete':   GObject.param_spec_boolean('delete',   '', '', false, GObject.ParamFlags.READWRITE),
+        'disabled': GObject.param_spec_boolean('disabled', '', '', false, GObject.ParamFlags.READWRITE),
+        'unpin':    GObject.param_spec_boolean('unpin',    '', '', false, GObject.ParamFlags.READWRITE),
+    },
+}, class ExtensionList extends GObject.Object {
     _init() {
         super._init();
-    }
-
-    get _url() {
-        return gsettings.get_boolean(Fields.URL);
-    }
-
-    get _prefs() {
-        return gsettings.get_boolean(Fields.PREFS);
-    }
-
-    get _delete() {
-        return gsettings.get_boolean(Fields.DELETE);
-    }
-
-    get _disabled() {
-        return gsettings.get_boolean(Fields.DISABLED);
-    }
-
-    get _unpin() {
-        return gsettings.get_boolean(Fields.UNPIN);
     }
 
     get _unpinlist() {
@@ -90,7 +77,7 @@ class ExtensionList extends GObject.Object {
         gsettings.set_strv(Fields.UNPINLIST, list);
     }
 
-    _addButton() {
+    _addIndicator() {
         this._button = new PanelMenu.Button(0.0, null, false);
         this._button.add_actor(new St.Icon({ icon_name: 'application-x-addon-symbolic', style_class: 'system-status-icon' }));
         Main.panel.addToStatusArea(Me.metadata.uuid, this._button, 0, 'right');
@@ -98,8 +85,8 @@ class ExtensionList extends GObject.Object {
 
     _menuItemMaker(ext) {
         let item = new PopupMenu.PopupBaseMenuItem({ style_class: 'extension-list-item' });
-        item.setOrnament(ext.state == ExtState.ENABLED && !this._disabled ? PopupMenu.Ornament.DOT : PopupMenu.Ornament.NONE);
-        let toggle = () => { item._ornament == PopupMenu.Ornament.NONE && !this._disabled ? ExtManager.enableExtension(ext.uuid) : ExtManager.disableExtension(ext.uuid); };
+        item.setOrnament(ext.state == ExtState.ENABLED && !this.disabled ? PopupMenu.Ornament.DOT : PopupMenu.Ornament.NONE);
+        let toggle = () => { item._ornament == PopupMenu.Ornament.NONE && !this.disabled ? ExtManager.enableExtension(ext.uuid) : ExtManager.disableExtension(ext.uuid); };
         item.connect('activate', () => { item._getTopMenu().close(); toggle(); });
         item.add_child(new St.Label({
             x_expand: true,
@@ -115,9 +102,9 @@ class ExtensionList extends GObject.Object {
             btn.connect('clicked', () => { item._getTopMenu().close(); func(); });
             hbox.add_child(btn);
         }
-        if(this._prefs && ext.hasPrefs) addButtonItem('emblem-system-symbolic', () => { ExtManager.openExtensionPrefs(ext.uuid, '', {}); });
-        if(this._url && ext.metadata.url) addButtonItem('mail-forward-symbolic', () => { Util.spawn(["gio", "open", ext.metadata.url]); });
-        if(this._delete && ext.type != ExtType.SYSTEM) addButtonItem('edit-delete-symbolic', () => { ExtDownloader.uninstallExtension(ext.uuid); this._updateMenu(); });
+        if(this.prefs && ext.hasPrefs) addButtonItem('emblem-system-symbolic', () => { ExtManager.openExtensionPrefs(ext.uuid, '', {}); });
+        if(this.url && ext.metadata.url) addButtonItem('mail-forward-symbolic', () => { Util.spawn(["gio", "open", ext.metadata.url]); });
+        if(this.delete && ext.type != ExtType.SYSTEM) addButtonItem('edit-delete-symbolic', () => { ExtDownloader.uninstallExtension(ext.uuid); this._updateMenu(); });
         item.add_child(hbox);
         return item;
     }
@@ -147,7 +134,7 @@ class ExtensionList extends GObject.Object {
                 this._unpinlist = [...list.add(ext.uuid)];
                 btn.child.icon_name = 'eye-not-looking-symbolic';
             }
-            if(this._unpin) gsettings.set_boolean(Fields.UNPIN, false);
+            if(this.unpin) this.unpin = false;
         }
         btn.connect('clicked', toggle);
         item.connect('activate', () => { toggle(); this._updateMenu(); });
@@ -167,9 +154,9 @@ class ExtensionList extends GObject.Object {
             });
             btn.connect('clicked', () => {
                 if(unpin) {
-                    gsettings.set_boolean(Fields.UNPIN, !this._unpin); func();
+                    this.unpin = !this.unpin; func();
                 } else {
-                    if(this._unpin) gsettings.set_boolean(Fields.UNPIN, false); func();
+                    if(this.unpin) this.unpin = false; func();
                 }
             });
             hbox.add_child(btn);
@@ -181,11 +168,11 @@ class ExtensionList extends GObject.Object {
         });
         if(gsettings.get_boolean(Fields.DEBUG))
             addButtonItem('applications-engineering-symbolic', () => { item._getTopMenu().close(); this._reloadShell(); });
-        addButtonItem('face-cool-symbolic', () => { gsettings.set_boolean(Fields.DISABLED, !this._disabled); this._updateMenu(); });
-        addButtonItem('emblem-system-symbolic', () => { this._singleton(!this._prefs, false, false); });
-        addButtonItem('mail-forward-symbolic', () => { this._singleton(false, !this._url, false); });
-        addButtonItem('edit-delete-symbolic', () => { this._singleton(false, false, !this._delete); });
-        if(!this._disabled)
+        addButtonItem('face-cool-symbolic', () => { this.disabled = !this.disabled; this._updateMenu(); });
+        addButtonItem('emblem-system-symbolic', () => { this._singleton(!this.prefs, false, false); });
+        addButtonItem('mail-forward-symbolic', () => { this._singleton(false, !this.url, false); });
+        addButtonItem('edit-delete-symbolic', () => { this._singleton(false, false, !this.delete); });
+        if(!this.disabled)
             addButtonItem('eye-open-negative-filled-symbolic', this._updateMenu.bind(this), true);
         item.add_child(hbox);
         return item;
@@ -200,16 +187,16 @@ class ExtensionList extends GObject.Object {
     }
 
     _singleton(x, y, z) {
-        gsettings.set_boolean(Fields.URL, y);
-        gsettings.set_boolean(Fields.PREFS, x);
-        gsettings.set_boolean(Fields.DELETE, z);
+        this.url = y;
+        this.prefs = x;
+        this.delete = z;
         this._updateMenu();
     }
 
     _updateMenu() {
         this._button.menu.removeAll();
         let scroll = new PopupScrollMenu();
-        if(this._unpin) {
+        if(this.unpin) {
             let list = this._unpinlist;
             ExtManager.getUuids()
                 .sort((x, y) => x.toLowerCase().localeCompare(y.toLowerCase()))
@@ -220,7 +207,7 @@ class ExtensionList extends GObject.Object {
                 .sort((x, y) => x.toLowerCase().localeCompare(y.toLowerCase()))
                 .filter(x => !this._unpinlist.has(x))
                 .map(x => ExtManager.lookup(x))
-                .filter(x => !this._disabled || x.state === ExtState.ENABLED)
+                .filter(x => !this.disabled || x.state === ExtState.ENABLED)
                 .forEach(x => { scroll.addMenuItem(this._menuItemMaker(x)); });
         }
         this._button.menu.addMenuItem(scroll);
@@ -228,14 +215,24 @@ class ExtensionList extends GObject.Object {
         this._button.menu.addMenuItem(this._settingItem());
     }
 
+    _bindSettings() {
+        gsettings.bind(Fields.DELETE,   this, 'delete',   Gio.SettingsBindFlags.DEFAULT);
+        gsettings.bind(Fields.DISABLED, this, 'disabled', Gio.SettingsBindFlags.DEFAULT);
+        gsettings.bind(Fields.PREFS,    this, 'prefs',    Gio.SettingsBindFlags.DEFAULT);
+        gsettings.bind(Fields.UNPIN,    this, 'unpin',    Gio.SettingsBindFlags.DEFAULT);
+        gsettings.bind(Fields.URL,      this, 'url',      Gio.SettingsBindFlags.DEFAULT);
+    }
+
     enable() {
-        this._addButton();
+        this._bindSettings();
+        this._addIndicator();
         this._stateChangeId = ExtManager.connect('extension-state-changed', this._updateMenu.bind(this));
     }
 
     disable() {
         if(this._stateChangeId) ExtManager.disconnect(this._stateChangeId), this._stateChangeId = 0;
         this._button.destroy();
+        this._button = null;
     }
 });
 
