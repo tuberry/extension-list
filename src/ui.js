60843e3e67225766e9473ed06f9f7a96dcb4fefb
tuberry <17917040+tuberry@users.noreply.github.com>
2023-12-29 12:43:59 +0800

    update

--- src/ui.js
+++ src/ui.js
@@ -11,7 +11,7 @@ import GObject from 'gi://GObject';
 import * as Gettext from 'gettext';
 import { Field } from './const.js';
 
-import { fopen, raise, omap, noop, gprops, fquery, hook, BIND_FULL } from './util.js';
+import { fopen, omap, noop, gprops, fquery, hook, BIND_FULL } from './util.js';
 import { ExtensionPreferences, gettext as _ } from 'resource:///org/gnome/Shell/Extensions/js/extensions/prefs.js';
 
 Gio._promisify(Gtk.FileDialog.prototype, 'open');
@@ -22,18 +22,18 @@ export const _GTK = Gettext.domain('gtk40').gettext;
 export const getSelf = () => ExtensionPreferences.lookupByURL(import.meta.url);
 export const block = (o, s) => omap(o, ([k, [x, y]]) => [[k, (s.bind(Field[k], y, x, Gio.SettingsBindFlags.DEFAULT), y)]]);
 
-export const Hook = new class Conns {
-    #map = new WeakMap();
-    attach(cbs, obj) {
+export class Hook {
+    static #map = new WeakMap();
+    static attach(cbs, obj) {
         this.detach(obj);
         this.#map.set(obj, cbs);
         return hook(cbs, obj);
     }
 
-    detach(obj) {
+    static detach(obj) {
         Object.values(this.#map.get(obj) ?? {}).forEach(x => GObject.signal_handlers_disconnect_by_func(obj, x));
     }
-}();
+}
 
 
 export class Prefs extends ExtensionPreferences {
@@ -59,9 +59,9 @@ export class Spin extends Gtk.SpinButton {
         GObject.registerClass(this);
     }
 
-    constructor(l, u, s, tip) {
-        super({ tooltip_text: tip || '', valign: Gtk.Align.CENTER });
-        this.set_adjustment(new Gtk.Adjustment({ lower: l, upper: u, step_increment: s }));
+    constructor(lower, upper, step_increment, tooltip_text = '') {
+        super({ tooltip_text, valign: Gtk.Align.CENTER });
+        this.set_adjustment(new Gtk.Adjustment({ lower, upper, step_increment }));
     }
 }
 
@@ -74,8 +74,8 @@ export class Drop extends Gtk.DropDown {
         }, this);
     }
 
-    constructor(opts, tip) {
-        super({ model: Gtk.StringList.new(opts), valign: Gtk.Align.CENTER, tooltip_text: tip || '' });
+    constructor(strv, tooltip_text = '') {
+        super({ model: Gtk.StringList.new(strv), valign: Gtk.Align.CENTER, tooltip_text });
         this.connect('notify::selected', () => this.emit('changed', this.selected));
     }
 }
@@ -221,10 +221,10 @@ export class KeysDialog extends DialogBase {
 
     _buildContent({ title }) {
         this.set_content(new Adw.StatusPage({ icon_name: 'preferences-desktop-keyboard-symbolic', title }));
-        this.add_controller(hook({ key_pressed: this._onKeyPressed.bind(this) }, new Gtk.EventControllerKey()));
+        this.add_controller(hook({ key_pressed: this._onKeyPress.bind(this) }, new Gtk.EventControllerKey()));
     }
 
-    _onKeyPressed(_w, keyval, keycode, state) {
+    _onKeyPress(_w, keyval, keycode, state) {
         let mask = state & Gtk.accelerator_get_default_mod_mask() & ~Gdk.ModifierType.LOCK_MASK;
         if(!mask && keyval === Gdk.KEY_Escape) return this.close();
         if(!this.isValidBinding(mask, keycode, keyval) || !this.isValidAccel(mask, keyval)) return;
@@ -282,16 +282,16 @@ class IconDialog extends DialogBase {
             model = Gtk.StringList.new(Gtk.IconTheme.get_for_display(Gdk.Display.get_default()).get_icon_names()),
             select = new Gtk.SingleSelection({ model: new Gtk.FilterListModel({ model, filter }) }),
             view = hook({ activate: () => this._onSelect() }, new Gtk.GridView({ model: select, factory, vexpand: true }));
-        filter.append(new Gtk.BoolFilter({ expression: this.icon_expression }));
+        filter.append(new Gtk.BoolFilter({ expression: this._genIconExp() }));
         filter.append(new Gtk.StringFilter({ expression: new Gtk.PropertyExpression(Gtk.StringObject, null, 'string') }));
-        this.connect('notify::icon-type', () => filter.get_item(0).set_expression(this.icon_expression));
+        this.connect('notify::icon-type', () => filter.get_item(0).set_expression(this._genIconExp()));
         this.bind_property('icon-type', title, 'selected', BIND_FULL);
         if(param?.icon_type) this.icon_type = param.icon_type;
         this.getSelected = () => select.get_selected_item().get_string();
         return { view, title, filter: filter.get_item(1) };
     }
 
-    get icon_expression() {
+    _genIconExp() {
         switch(this.icon_type) {
         case 1: return new Gtk.ClosureExpression(GObject.TYPE_BOOLEAN, x => !x.string.endsWith('-symbolic'), null);
         case 2: return new Gtk.ClosureExpression(GObject.TYPE_BOOLEAN, x => x.string.endsWith('-symbolic'), null);
@@ -327,13 +327,12 @@ export class DialogButtonBase extends Box {
     }
 
     _buildDND(gtype) {
-        let drop = hook({ drop: this._onDrop.bind(this) }, Gtk.DropTarget.new(gtype, Gdk.DragAction.COPY));
-        let drag = hook({ prepare: this._onDrag.bind(this) }, new Gtk.DragSource({ actions: Gdk.DragAction.COPY }));
-        [drop, drag].forEach(x => this._btn.add_controller(x));
+        this._btn.add_controller(hook({ drop: this._onDrop.bind(this) }, Gtk.DropTarget.new(gtype, Gdk.DragAction.COPY)));
+        this._btn.add_controller(hook({ prepare: this._onDrag.bind(this) }, new Gtk.DragSource({ actions: Gdk.DragAction.COPY })));
     }
 
     _onDrag(src) {
-        let icon = this._paintable;
+        let icon = this._genDragSwatch();
         if(icon) src.set_icon(icon, 0, 0);
         return Gdk.ContentProvider.new_for_value(this._gvalue);
     }
@@ -388,7 +387,7 @@ export class App extends DialogButtonBase {
         else this._btn.child.setupContent();
     }
 
-    get _paintable() {
+    _genDragSwatch() {
         return Gtk.IconTheme.get_for_display(Gdk.Display.get_default())
             .lookup_by_gicon(this._gvalue.get_icon(), 32, 1, Gtk.TextDirection.NONE, Gtk.IconLookupFlags.FORCE_SVG);
     }
@@ -421,7 +420,7 @@ export class File extends DialogButtonBase {
             this.value = value;
         } else {
             fquery(value, Gio.FILE_ATTRIBUTE_STANDARD_CONTENT_TYPE).then(y => {
-                if(this._filter.match(y)) this.value = value; else raise();
+                if(this._filter.match(y)) this.value = value; else throw Error();
             }).catch(() => {
                 this.get_root().add_toast(new Adw.Toast({ title: _('Mismatched filetype'), timeout: 5 }));
             });
@@ -457,7 +456,7 @@ export class Icon extends DialogButtonBase {
         super(new IconLabel('image-missing'), Gio.ThemedIcon.$gtype, true);
     }
 
-    get _paintable() {
+    _genDragSwatch() {
         return Gtk.IconTheme.get_for_display(Gdk.Display.get_default())
             .lookup_by_gicon(this._gvalue, 32, 1, Gtk.TextDirection.NONE, Gtk.IconLookupFlags.FORCE_SVG);
     }
@@ -550,18 +549,18 @@ export class LazyEntry extends Gtk.Stack {
         }, this);
     }
 
-    constructor(holder, tip) {
+    constructor(placeholder, tooltip) {
         super({ valign: Gtk.Align.CENTER, hhomogeneous: true });
-        this._buildWidgets(holder, tip);
+        this._buildWidgets(placeholder, tooltip);
         this.value = '';
     }
 
-    _buildWidgets(holder, tip) {
-        this._label = new Gtk.Entry({ hexpand: true, sensitive: false, placeholder_text: holder || '' });
+    _buildWidgets(placeholder_text = '', tooltip_text = '') {
+        this._label = new Gtk.Entry({ hexpand: true, sensitive: false, placeholder_text });
         this._entry = hook({ activate: () => { this.value = this._entry.get_text(); } },
-            new Gtk.Entry({ hexpand: true, enable_undo: true, placeholder_text: holder || '' }));
+            new Gtk.Entry({ hexpand: true, enable_undo: true, placeholder_text }));
         this._edit = hook({ clicked: () => { this._entry.set_text(this.value); this.set_visible_child_name('entry'); } },
-            new Gtk.Button({ icon_name: 'document-edit-symbolic', tooltip_text: tip || '' }));
+            new Gtk.Button({ icon_name: 'document-edit-symbolic', tooltip_text }));
         this._done = hook({ clicked: () => { this.value = this._entry.get_text(); } },
             new Gtk.Button({ icon_name: 'object-select-symbolic', tooltip_text: _('Click or press ENTER to commit changes') }));
         this.add_named(new Box([this._label, this._edit], { hexpand: true }), 'label');
